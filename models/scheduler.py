5e959a87a3b477c4e44af71fd54985ce5413fd34
davidorme <d.orme@imperial.ac.uk>
2017-08-01 13:16:05 +0100

    Forgot to save the file before committing

--- models/scheduler.py
+++ models/scheduler.py
@@ -140,7 +140,7 @@ def verify_dataset(id, email=False):
             fname = os.path.join(request.folder,'uploads','datasets', record.file)
             # use a local locations file - there is some issue with using the service from within the code
             locs = os.path.join(request.folder,'static','files','locations.json')
-            check_results = safe_dataset_checker.check_file(fname, verbose=False)
+            check_results = safe_dataset_checker.check_file(fname, verbose=False, locations_json=locs)
             
             # get the report
             messages = check_results['messages']
